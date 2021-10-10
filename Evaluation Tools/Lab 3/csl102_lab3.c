a789df2a2ead2cf8a12081527ed364ddbfe2b6aa
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-10 13:11:05 +0530

    Add files via upload

---
+++ Evaluation Tools/Lab 3/csl102_lab3.c
@@ -0,0 +1,20 @@
+#include "csl102_lab3.h"
+/* Check header file for documentation on these functions*/
+
+unsigned long csl102_diff_k_median(short array[], unsigned short szArrunsigned short k)
+{
+    /**
+     * Write your code here
+     * */
+    
+    return 0;
+}
+
+void csl102_sort_lab3(short arrayOne[], unsigned short szArrOne, short arrayTwo[], unsigned short szArrTwo)
+{
+     /**
+     * Write your code here
+     * */
+
+    return;
+}
